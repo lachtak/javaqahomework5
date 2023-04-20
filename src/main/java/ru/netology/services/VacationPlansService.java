4397575fe05432fc4bd64fe3ba89ec86aff81995
lachtak <lazynewocean@gmail.com>
2023-04-20 17:31:48 +0300

    VacationPlans

--- src/main/java/ru/netology/services/VacationPlansService.java
+++ src/main/java/ru/netology/services/VacationPlansService.java
@@ -1,3 +1,22 @@
-package 
+package ru.netology.services;
+
 public class VacationPlansService {
+
+    public int calculate(int income, int expense, int threshold) {
+        int count = 0; // Счётчик месяцев отдыха
+        int money = 0;// Количество денег на счету
+
+        for (int month = 0; month < 12; month++) {
+            if (money >= threshold) {
+                count++;
+                money = ((money + (income - expense)) - expense) / 3;
+            }
+            else {
+                money = money+(income - expense);
+            }
+
+        }
+        return count;
+    }
 }
+
--- src/test/java/ru/netology/services/VacationPlansServiceTest.java
+++ src/test/java/ru/netology/services/VacationPlansServiceTest.java
@@ -1,16 +1,25 @@
+package ru.netology.services;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.netology.services.VacationPlansService;
 
 public class VacationPlansServiceTest {
-
     @Test
-    void vacmonth () {
+    public void vacmonth() {
         VacationPlansService service = new VacationPlansService();
 
         int expected = 3;
+        int actual = service.calculate(10_000, 3000, 20_000);
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void vacmonth2() {
 
-        int actual = service.calculate(10000, 3000);
+        VacationPlansService service = new VacationPlansService();
+
+        int expected = 2;
+        int actual = service.calculate(100_000, 60_000, 150_000);
         Assertions.assertEquals(expected, actual);
     }
 }
